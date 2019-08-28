54b603940bb1bb275c85197bcf5c62b9a040b436
Taka <hikijiri@throo.io>
2019-08-29 02:00:32 +0900

    いろいろ

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/new'
 
-  devise_for :users, :controllers => {
-  :registrations => 'users/registrations',
-  :sessions => 'users/sessions'   
-} 
-
-devise_scope :user do
-  get "user/:id", :to => "users/registrations#detail"
-  get "signup", :to => "users/registrations#new"
-  get "login", :to => "users/sessions#new"
-  get "logout", :to => "users/sessions#destroy"
-end
-
-	root 'users#index'
+	root 'catches#index'
   resources :catches
   resources :prefectures
   resources :points
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
 
 # Prefecture.create!(name: "福岡", area_id: 1)
-Catch.create!(size:100, prefecture_id: 4)
+# Point.create!(name: "博多湾")
+Catch.create!(size:100, prefecture_id: 4, Text: "いいかんじ")
