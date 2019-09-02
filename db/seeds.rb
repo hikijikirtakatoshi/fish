f1ecced95ddeaf8624f3b7f4fe67f3c02bf04a65
Taka <hikijiri@throo.io>
2019-09-02 14:16:54 +0900

    seedsの修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,12 @@
 require 'csv'
 
-csv_data = CSV.read(Rails.root.to_s + "/Prefectures.csv")
+["北海道", "東北", "関東", "中部", "近畿", "中国", "四国", "九州", "沖縄"].each do |area| 
+	Area.create!(name: area) if Area.find_by(name: area) == nil
+end
 
+csv_data = CSV.read(Rails.root.to_s + "/Prefectures.csv")
 csv_data.each do |prefecture| 
 	Prefecture.create!(name: prefecture[1], area_id: 1) if Prefecture.find_by(name: prefecture[1]) == nil
 end
 
-
-["北海道", "東北", "関東", "中部", "近畿", "中国", "四国", "九州", "沖縄"].each do |area| 
-	Area.create!(name: area) if Area.find_by(name: area) == nil
-end
 
